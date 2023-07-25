5b6c7a60881245a94a0d699adbae79c791ac77b2
Alexander Martins <alexandermartins123@icloud.com>
2023-07-25 10:27:31 +0100

    handle validation and dupilcate field error

--- middleware/error.js
+++ middleware/error.js
@@ -2,16 +2,30 @@ const ErrorResponse = require("../utils/errorResponse");
 
 const errorHandler = (err, req, res, next) => {
   let error = { ...err };
+
   error.message = err.message;
 
   //  Log to console for dev
-  console.log(err.stack.red);
+  console.log(err);
 
   // Mongoose bad object
   if (err.name === "CastError") {
     const message = `Bootcamp not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
   }
+
+  // Mongoose Duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
   res.status(error.statusCode || 500).json({
     sucess: false,
     error: error.message || "Server Error",
